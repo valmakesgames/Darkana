694c77f8e304193ab474236ed1e2e6f32241800b
Val Nunez <val@friendlyhello.com>
2026-05-07 21:48:08 -0700

    Working on a test script to refresh my Unity/C# memory

--- dev_darkana/Assets/Scripts/HelloWorld.cs
+++ dev_darkana/Assets/Scripts/HelloWorld.cs
@@ -1,23 +1,46 @@
+using System;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
 public class HelloWorld : MonoBehaviour
 {
-    [SerializeField]
-    TMP_Text text;
-    [SerializeField]
+    [SerializeField] TMP_Text text;
+
     public Button enterButton;
-    
+
+    // This will hold a reference to the cube's material
+    private Material cubeMaterial;
+
+    private void Awake()
+    {
+        GameObject cube = GameObject.Find("Cube");
+        Debug.Log("Found object: " + cube);
+
+        if (cube != null)
+        {
+            cubeMaterial = cube.GetComponent<Renderer>().material;
+            Debug.Log("Got material: " + cubeMaterial);
+        } else {
+            Debug.Log(cube + "NOT FOUND.");
+        }
+    }
+
+    private void Update()
+    {
+        enterButton.transform.Rotate(0, 0, 0.10f);
+    }
+
     void Start()
     {
         Debug.Log("Hello, beautiful world!");
-        
         enterButton.onClick.AddListener(ButtonTest);
     }
 
     private void ButtonTest()
     {
         text.text = "This is a test";
+        
+        cubeMaterial.color = Color.green;
     }
 }
