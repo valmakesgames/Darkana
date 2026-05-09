a04a86f8fbb33b69bf64c29e36e347665d1104f7
Val Nunez <val@friendlyhello.com>
2026-05-08 21:08:42 -0700

    Removing unused sprite assets
    1. Removed unused sprite assets
    2. Testing new input system for car movement

--- dev_darkana/Assets/Scripts/Driver.cs
+++ dev_darkana/Assets/Scripts/Driver.cs
@@ -1,6 +1,43 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField]
+    float steerSpeed;
+    [SerializeField]
+    float moveSpeed;
     
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.wKey.isPressed)
+        {
+            transform.Translate(0, moveSpeed, 0);
+            Debug.Log("FORWARD (W key pressed)");
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            Debug.Log("LEFT (A key pressed)");
+        }
+
+        if (Keyboard.current.sKey.isPressed)
+        {
+            Debug.Log("DOWN (S key pressed)");
+        }
+
+        if (Keyboard.current.dKey.isPressed)
+        {
+            Debug.Log("RIGHT (D key pressed)");
+        }
+        
+        // transform.Rotate(0, 0, steerSpeed);
+        // transform.Translate(0, moveSpeed, 0);
+    }
 }
