c9810093c16102e695a63b6731a96ca0da4431cc
Val Nunez <val@friendlyhello.com>
2026-05-10 22:54:46 -0700

    Tail lights turn off when isMoving is true and off when isMoving is false

--- dev_darkana/Assets/Scripts/Driver.cs
+++ dev_darkana/Assets/Scripts/Driver.cs
@@ -9,31 +9,52 @@ public class Driver : MonoBehaviour
     [SerializeField]
     float steerSpeed;
 
-    private void Update() {
+    [SerializeField]
+    private GameObject truckTailLights;
+
+    private bool isMoving = false;
+
+    void Awake() {
+            truckTailLights.SetActive(true);
+            Debug.Log("Tail lights ON!");
+    }
+    
+    void Update() {
         float move = 0;
         float steer = 0;
         
-        // isMoving bool set to true, SetActive on to control tail lights
-        // if W || S || A || D is pressed
-        // SetActive HDR tail lights off
+        isMoving = false;
         
         // WASD Movement/Steering
         if (Keyboard.current.wKey.isPressed) {
+            isMoving = true; 
+            Debug.Log("Tail lights OFF!")
             move = 1f;
             Debug.Log("FORWARD (W key pressed)");
-        } else if 
+        } else if
             (Keyboard.current.sKey.isPressed) {
+            isMoving = true;
+            Debug.Log("Tail lights OFF!")
             move = -1f;
             Debug.Log("DOWN (S key pressed)");
         } 
+        
         if (Keyboard.current.aKey.isPressed) { 
+            isMoving = true;
             steer = 1f;
             Debug.Log("LEFT (A key pressed)");
         } else if 
             (Keyboard.current.dKey.isPressed) {
+            isMoving = true;
             steer = -1f;
             Debug.Log("RIGHT (D key pressed)");
         } 
+        
+        if (isMoving) {
+            truckTailLights.SetActive(false);
+        } else {
+            truckTailLights.SetActive(true);
+        }
 
         float moveAmount = move * moveSpeed * Time.deltaTime;
         float steerAmount =  steer * steerSpeed * Time.deltaTime;
