d859161d4936fab19fae23d3e83a39746e022ee2
Val Nunez <val@friendlyhello.com>
2026-05-13 12:30:42 -0700

    playing w/Collision
    Attached Collision class/script to truck prefab and tested collision

--- dev_darkana/Assets/Scripts/Collision.cs
+++ dev_darkana/Assets/Scripts/Collision.cs
@@ -1,6 +1,10 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-   
+    private void OnCollisionEnter2D() {
+        Debug.Log(gameObject.name + " collided! ");
+    }
 }
