7b00e014d821b84ff45ad711f07660b17984a0be
Val Nunez <val@friendlyhello.com>
2026-05-13 19:32:33 -0700

    Added test trigger volume
    1. Added test trigger volume
    2. Added trigger volume prefab
    3. Updated sprite layer order for truck and tail light prefabs from 0 to 1.

--- dev_darkana/Assets/Scripts/Collision.cs
+++ dev_darkana/Assets/Scripts/Collision.cs
@@ -7,4 +7,8 @@ public class Collision : MonoBehaviour
     private void OnCollisionEnter2D() {
         Debug.Log(gameObject.name + " collided! ");
     }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Triggered " + other.name);
+    }
 }
